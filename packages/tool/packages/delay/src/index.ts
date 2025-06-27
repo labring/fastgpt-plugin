3afaaec1afbc3cf4984a42d420fe470ebac9cf6f
archer <545436317@qq.com>
2025-06-27 13:01:44 +0800

    fix delay

--- packages/tool/packages/delay/src/index.ts
+++ packages/tool/packages/delay/src/index.ts
@@ -1,4 +1,3 @@
-import { addLog } from '@/utils/log';
 import { z } from 'zod';
 
 export const InputType = z
@@ -6,21 +5,16 @@ export const InputType = z
     ms: z.number().min(1).max(300000).optional(), // 延迟时长，单位毫秒
     延时时长: z.number().min(1).max(300000).optional()
   })
-  .refine((data) => {
-    return data.ms || data.延时时长 || 1;
-  })
   .transform((data) => {
+    console.log(data);
     return {
-      ...data,
-      ms: data.ms || data.延时时长
+      ms: data.ms || data.延时时长 || 1
     };
   });
 
 export const OutputType = z.object({});
 
-export async function tool({
-  ms = 1
-}: z.infer<typeof InputType>): Promise<z.infer<typeof OutputType>> {
+export async function tool({ ms }: z.infer<typeof InputType>): Promise<z.infer<typeof OutputType>> {
   // delay for ms milliseconds
   await new Promise((resolve) => setTimeout(resolve, ms));
   return {};
