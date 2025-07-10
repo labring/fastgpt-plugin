ad6db6d7f18dfa74f9152054080f8be3b2bf128e
FinleyGe <m13203533462@163.com>
2025-07-10 17:59:06 +0800

    feat: support system-toolset

--- modules/tool/packages/Doc2X/config.ts
+++ modules/tool/packages/Doc2X/config.ts
@@ -1,5 +1,6 @@
 import { defineToolSet } from '@tool/type';
 import { ToolTypeEnum } from '@tool/type/tool';
+import { defineInputConfig } from '@tool/utils/tool';
 
 export default defineToolSet({
   name: {
@@ -12,5 +13,14 @@ export default defineToolSet({
     'zh-CN': '将传入的图片或PDF文件发送至Doc2X进行解析，返回带LaTeX公式的markdown格式的文本。',
     en: 'Send an image or PDF file to Doc2X for parsing and return the LaTeX formula in markdown format.'
   },
-  icon: 'plugins/doc2x'
+  icon: 'plugins/doc2x',
+  inputConfig: defineInputConfig([
+    {
+      key: 'apikey',
+      label: 'apikey',
+      description: 'Doc2X的API密钥，可以从Doc2X开放平台获得',
+      required: true,
+      inputType: 'secret'
+    }
+  ])
 });
