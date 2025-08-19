46f5c4704778337449e5cbc665560df8ea57db5f
ctrlz526 <3167694193@qq.com>
2025-08-19 10:28:01 +0800

    feat: add ToolTypeTranslations for multilingual support in tool types

--- sdk/client.ts
+++ sdk/client.ts
@@ -1,10 +1,11 @@
 import createClient from '@/contract/client';
 import type { SystemVarType } from '@tool/type/tool';
 import type { StreamMessageType } from '@tool/type/tool';
-
 export default createClient;
+import { ToolTypeEnum, ToolTypeTranslations } from '@tool/type/tool';
 
 export type { SystemVarType, StreamMessageType as StreamMessage };
-
 export { RunToolWithStream } from './runToolStream';
 export { StreamDataAnswerTypeEnum } from '@tool/type/tool';
+
+export { ToolTypeEnum, ToolTypeTranslations };
