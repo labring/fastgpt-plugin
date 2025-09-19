90d4c267e8a3d89429be3ae4ba236d26df58dcad
FinleyGe <m13203533462@163.com>
2025-09-19 15:52:26 +0800

    chore: sdk export upload tools path

--- modules/tool/constants.ts
+++ modules/tool/constants.ts
@@ -2,3 +2,5 @@ import type { ToolType } from './type';
 
 export const uploadedTools: ToolType[] = [];
 export const builtinTools: ToolType[] = [];
+
+export const UploadToolsS3Path = 'system/tools';
