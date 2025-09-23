d25a54f72ba3ace3c3302382669ffb1c536a66e2
archer <545436317@qq.com>
2025-09-23 16:07:50 +0800

    perf: s3 checker

--- modules/tool/cronTask/cleanOrphanPlugins.ts
+++ modules/tool/cronTask/cleanOrphanPlugins.ts
@@ -3,6 +3,8 @@ import { lockEnum, withLock } from '@/redis/lock';
 import { pluginFileS3Server } from '@/s3';
 import { addLog } from '@/utils/log';
 import { UploadToolsS3Path } from '@tool/constants';
+
+// Remove invalid s3 files
 export default async () => {
   try {
     await withLock(lockEnum.Enum.cleanOrphanPlugin, 60000, async () => {
@@ -10,9 +12,10 @@ export default async () => {
         type: pluginTypeEnum.Enum.tool
       }).lean();
 
-      const objectNames = tools.map((tool) => tool.objectName);
+      const objectNames = tools.reduce((acc, tool) => acc.add(tool.objectName), new Set<string>());
       const files = await pluginFileS3Server.getFiles(UploadToolsS3Path);
-      const orphans = files.filter((file) => !objectNames.includes(file));
+
+      const orphans = files.filter((file) => !objectNames.has(file));
       await pluginFileS3Server.removeFiles(orphans);
     });
   } catch {
