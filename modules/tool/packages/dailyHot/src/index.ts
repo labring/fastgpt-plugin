77b7a314b4452fa8d9ec9e2ab84f855c5369e592
archer <545436317@qq.com>
2025-10-16 21:59:27 +0800

    perf: code

--- modules/tool/packages/dailyHot/src/index.ts
+++ modules/tool/packages/dailyHot/src/index.ts
@@ -7,23 +7,22 @@ export const InputType = z.object({
 
 export const OutputType = z.object({
   hotNewsList: z.array(
-    z.object({
-      title: z.string().describe('hot news title').optional(),
-      description: z.string().describe('hot news description').optional(),
-      source: z.string().describe('hot news source website'),
-      time: z.string().describe('hot news publish time').optional(),
-      error: z.string().describe('failed to get hot news, error message').optional()
-    })
+    z.union([
+      z.object({
+        title: z.string().describe('hot news title').optional(),
+        description: z.string().describe('hot news description').optional(),
+        source: z.string().describe('hot news source website'),
+        time: z.string().describe('hot news publish time').optional()
+      }),
+      z.object({
+        source: z.string().describe('hot news source website'),
+        error: z.string().describe('failed to get hot news, error message').optional()
+      })
+    ])
   )
 });
 
-type Response = {
-  title?: string;
-  description?: string;
-  source: string;
-  time?: string;
-  error?: string;
-};
+type Response = z.infer<typeof OutputType>['hotNewsList'][number];
 
 type Kr36Response = {
   data: {
@@ -35,93 +34,6 @@ type Kr36Response = {
     }[];
   };
 };
-
-type ZhihuResponse = {
-  data: {
-    target: {
-      title: string;
-      created: number;
-      excerpt: string;
-    };
-  }[];
-};
-
-type WeiboResponse = {
-  data: {
-    cards: {
-      card_group?: {
-        desc: string;
-      }[];
-    }[];
-  };
-};
-
-type JuejinResponse = {
-  data: {
-    content: {
-      content_id: string;
-      title: string;
-      created_at: number;
-      ctime: number;
-    };
-    author: {
-      name: string;
-    };
-    content_counter: {
-      hot_rank: number;
-    };
-  }[];
-};
-
-type ToutiaoResponse = {
-  data: {
-    Title: string;
-  }[];
-};
-
-export async function tool({
-  sources
-}: z.infer<typeof InputType>): Promise<z.infer<typeof OutputType>> {
-  const promises = sources.map(async (source) => {
-    switch (source) {
-      case '36kr':
-        return await get36krList();
-      case 'zhihu':
-        return await getZhihuList();
-      case 'weibo':
-        return await getWeiboList();
-      case 'juejin':
-        return await getJuejinList();
-      case 'toutiao':
-        return await getToutiaoList();
-      default:
-        return [];
-    }
-  });
-  const results = await Promise.allSettled(promises);
-  const allHotNewsList: Response[] = [];
-  results.forEach((result, index) => {
-    if (result.status === 'fulfilled') {
-      allHotNewsList.push(...result.value);
-    } else {
-      allHotNewsList.push({
-        error: `Failed to get hot news from ${sources[index]}`,
-        source: sources[index]
-      });
-    }
-  });
-
-  if (!allHotNewsList.length) {
-    return Promise.reject({
-      error: 'Failed to get hot news list'
-    });
-  }
-
-  return {
-    hotNewsList: allHotNewsList
-  };
-}
-
 async function get36krList(): Promise<Response[]> {
   const url = `https://gateway.36kr.com/api/mis/nav/home/nav/rank/hot`;
 
@@ -151,6 +63,15 @@ async function get36krList(): Promise<Response[]> {
   return hotNewsList;
 }
 
+type ZhihuResponse = {
+  data: {
+    target: {
+      title: string;
+      created: number;
+      excerpt: string;
+    };
+  }[];
+};
 async function getZhihuList(): Promise<Response[]> {
   const url = `https://api.zhihu.com/topstory/hot-lists/total?limit=50`;
 
@@ -175,6 +96,15 @@ async function getZhihuList(): Promise<Response[]> {
   return hotNewsList;
 }
 
+type WeiboResponse = {
+  data: {
+    cards: {
+      card_group?: {
+        desc: string;
+      }[];
+    }[];
+  };
+};
 async function getWeiboList(): Promise<Response[]> {
   const url =
     'https://m.weibo.cn/api/container/getIndex?containerid=106003type%3D25%26t%3D3%26disable_hot%3D1%26filter_type%3Drealtimehot&title=%E5%BE%AE%E5%8D%9A%E7%83%AD%E6%90%9C&extparam=filter_type%3Drealtimehot%26mi_cid%3D100103%26pos%3D0_0%26c_type%3D30%26display_time%3D1540538388&luicode=10000011&lfid=231583';
@@ -201,6 +131,22 @@ async function getWeiboList(): Promise<Response[]> {
   return hotNewsList;
 }
 
+type JuejinResponse = {
+  data: {
+    content: {
+      content_id: string;
+      title: string;
+      created_at: number;
+      ctime: number;
+    };
+    author: {
+      name: string;
+    };
+    content_counter: {
+      hot_rank: number;
+    };
+  }[];
+};
 async function getJuejinList(): Promise<Response[]> {
   const url = 'https://api.juejin.cn/content_api/v1/content/article_rank?category_id=1&type=hot';
 
@@ -222,6 +168,11 @@ async function getJuejinList(): Promise<Response[]> {
   return hotNewsList;
 }
 
+type ToutiaoResponse = {
+  data: {
+    Title: string;
+  }[];
+};
 async function getToutiaoList(): Promise<Response[]> {
   const url = `https://www.toutiao.com/hot-event/hot-board/?origin=toutiao_pc`;
 
@@ -240,4 +191,42 @@ async function getToutiaoList(): Promise<Response[]> {
   });
 
   return hotNewsList;
+}
+
+export async function tool({
+  sources
+}: z.infer<typeof InputType>): Promise<z.infer<typeof OutputType>> {
+  const map = {
+    '36kr': get36krList,
+    zhihu: getZhihuList,
+    weibo: getWeiboList,
+    juejin: getJuejinList,
+    toutiao: getToutiaoList
+  };
+  const promises = sources.map(async (source) => {
+    return await map[source]();
+  });
+
+  const results = await Promise.allSettled(promises);
+  const allHotNewsList: Response[] = [];
+  results.forEach((result, index) => {
+    if (result.status === 'fulfilled') {
+      allHotNewsList.push(...result.value);
+    } else {
+      allHotNewsList.push({
+        error: `Failed to get hot news from ${sources[index]}`,
+        source: sources[index]
+      });
+    }
+  });
+
+  if (!allHotNewsList.length) {
+    return Promise.reject({
+      error: 'Failed to get hot news list'
+    });
+  }
+
+  return {
+    hotNewsList: allHotNewsList
+  };
 }
