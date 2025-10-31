f19b843499076c12505bbf87eab58f3ee70d0a5a
archer <545436317@qq.com>
2025-10-31 20:25:21 +0800

    reset sdk

--- sdk/build.ts
+++ sdk/build.ts
@@ -3,7 +3,7 @@ import dts from 'bun-plugin-dts';
 const result = await Bun.build({
   entrypoints: ['./client.ts'],
   target: 'node',
-  external: ['zod'],
+  // external: ['zod'],
   outdir: './dist',
   plugins: [dts()]
 });
