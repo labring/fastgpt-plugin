d682285f59f8e89d525f9c06fde69159a3cede5e
Finley Ge <finleyge@fastgpt.io>
2026-01-04 21:09:45 +0800

    chore: restore the index.ts file to template raw

--- modules/tool/packages/wecomSmartSheet/index.ts
+++ modules/tool/packages/wecomSmartSheet/index.ts
@@ -1,25 +1,8 @@
+// You  should not modify this file, if you need to modify the tool set configuration, please modify the config.ts file
+
 import config from './config';
 import { exportToolSet } from '@tool/utils/tool';
 
-import wecomSmartSheetDoc from './children/wecomSmartSheetDoc';
-import wecomSmartSheetFieldAdvanced from './children/wecomSmartSheetFieldAdvanced';
-import wecomSmartSheetFieldSimple from './children/wecomSmartSheetFieldSimple';
-import wecomSmartSheetRecordAdvanced from './children/wecomSmartSheetRecordAdvanced';
-import wecomSmartSheetRecordSimple from './children/wecomSmartSheetRecordSimple';
-import wecomSmartSheetTable from './children/wecomSmartSheetTable';
-import wecomSmartSheetView from './children/wecomSmartSheetView';
-
 export default exportToolSet({
-  config: {
-    ...config,
-    children: [
-      wecomSmartSheetDoc,
-      wecomSmartSheetFieldAdvanced,
-      wecomSmartSheetFieldSimple,
-      wecomSmartSheetRecordAdvanced,
-      wecomSmartSheetRecordSimple,
-      wecomSmartSheetTable,
-      wecomSmartSheetView
-    ]
-  }
+  config
 });
