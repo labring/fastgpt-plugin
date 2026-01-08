91e2800309d098aa05281f0253451d0d4287367e
Finley Ge <finleyge@fastgpt.io>
2026-01-08 16:21:05 +0800

    fix: invoke in main thread

--- modules/tool/packages/wecomCorpId/src/index.ts
+++ modules/tool/packages/wecomCorpId/src/index.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
-import { invoke } from '@/invoke';
-import type { getCorpTokenResult } from '@/invoke/wecom/getAuthToken';
+import { getCorpToken } from '@/invoke/wecom/getAuthToken';
+import type { RunToolSecondParamsType } from '@tool/type/req';
 
 export const InputType = z.object({});
 
@@ -9,9 +9,13 @@ export const OutputType = z.object({
   expires_in: z.number()
 });
 
-export async function tool(): Promise<z.infer<typeof OutputType>> {
+export async function tool(
+  _params: z.infer<typeof InputType>,
+  { systemVar }: RunToolSecondParamsType
+): Promise<z.infer<typeof OutputType>> {
   // 调用 wecom.getAuthToken 获取企业微信授权 token
-  const result = await invoke<getCorpTokenResult>('wecom.getCorpToken');
+  // const result = await invoke<getCorpTokenResult>('wecom.getCorpToken');
+  const result = await getCorpToken({}, systemVar);
 
   return {
     access_token: result.access_token,
